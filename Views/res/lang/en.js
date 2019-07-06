2147b5b836ad17ec4b26a264a7fe46b091c4577b
MarkoTheStudent <marko.gluhak@student.um.si>
2019-07-06 21:11:30 +0200

    Improved language support, increased responsiveness, added styling o the navbar

--- Views/res/lang/en.js
+++ Views/res/lang/en.js
@@ -8,6 +8,10 @@ const navigation = {
 	language: { name: "Language" }
 };
 
+const global = {
+	more1: "More..."
+};
+
 module.exports = {
 	Home: {
 		layout: "main",
@@ -15,9 +19,24 @@ module.exports = {
 		css: "/style/homepage.css",
 		navigation,
 		heading1: "Experience with us",
-		expo1: "Saling School",
-		expo2: "Sailing Trips",
-		expo3: "Teambuilding"
+		expo1: {
+			title: "Saling School",
+			text:
+				"Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.",
+			more: global.more1
+		},
+		expo2: {
+			title: "Sailing Trips",
+			text:
+				"Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.",
+			more: global.more1
+		},
+		expo3: {
+			title: "Teambuilding",
+			text:
+				"Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.",
+			more: global.more1
+		}
 	},
 	About: {
 		layout: "main",
--- Views/res/lang/sl.js
+++ Views/res/lang/sl.js
@@ -8,6 +8,10 @@ const navigation = {
 	language: { name: "Language" }
 };
 
+const global = {
+	more1: "Več..."
+};
+
 module.exports = {
 	Home: {
 		layout: "main",
@@ -15,9 +19,24 @@ module.exports = {
 		css: "/style/homepage.css",
 		navigation,
 		heading1: "Doživite z nami",
-		expo1: "Šola Jadranja",
-		expo2: "Jadralski Izleti",
-		expo3: "Teambuilding"
+		expo1: {
+			title: "Šola Jadranja",
+			text:
+				"Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.",
+			more: global.more1
+		},
+		expo2: {
+			title: "Jadralski Izleti",
+			text:
+				"Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.",
+			more: global.more1
+		},
+		expo3: {
+			title: "Teambuilding",
+			text:
+				"Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.",
+			more: global.more1
+		}
 	},
 	About: {
 		layout: "main",
