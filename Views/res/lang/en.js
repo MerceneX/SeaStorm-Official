0f3227f6bac7b8d41bdf9f17cc972e35a46137b7
MarkoTheStudent <marko.gluhak@student.um.si>
2019-07-07 20:30:15 +0200

    Begun adding content, actualized entire navbar with full dual language support

--- Views/res/lang/en.js
+++ Views/res/lang/en.js
@@ -1,9 +1,36 @@
 const navigation = {
 	home: { name: "Home", link: "/en" },
-	sschool: { name: "Sailing School", link: "/en" },
-	strips: { name: "Sailing Trips", link: "/en" },
+	sschool: {
+		name: "Sailing School",
+		link: "/en",
+		dropdown: [
+			{ name: "Beginner Course", link: "#" },
+			{ name: "Intermediate Course", link: "#" },
+			{ name: "Individual Course", link: "#" },
+			{ name: "Boat Leader Course", link: "#" }
+		]
+	},
+	strips: {
+		name: "Sailing Trips",
+		link: "/en",
+		dropdown: [
+			{ name: "4h Trip", link: "#" },
+			{ name: "1 Day Trip", link: "#" },
+			{ name: "2 Day Trip", link: "#" },
+			{ name: "3 Day Trip", link: "#" },
+			{ name: "Weekly Trip", link: "#" }
+		]
+	},
 	tbuild: { name: "Team Building", link: "/en" },
-	slit: { name: "Literature", link: "/en" },
+	slit: {
+		name: "Literature",
+		link: "/en",
+		dropdown: [
+			{ name: "Navigation", link: "#" },
+			{ name: "Manouvering", link: "#" },
+			{ name: "Maintenance", link: "#" }
+		]
+	},
 	about: { name: "About", link: "/en/about" },
 	language: { name: "Language" }
 };
@@ -15,7 +42,7 @@ const global = {
 module.exports = {
 	Home: {
 		layout: "main",
-		title: "Home",
+		title: "SeaStorm Sailing",
 		css: "/style/homepage.css",
 		navigation,
 		heading1: "Experience with us",
--- Views/res/lang/sl.js
+++ Views/res/lang/sl.js
@@ -1,9 +1,36 @@
 const navigation = {
 	home: { name: "Domov", link: "/sl" },
-	sschool: { name: "Šola Jadranja", link: "/sl" },
-	strips: { name: "Jadralski Izleti", link: "/sl" },
+	sschool: {
+		name: "Šola Jadranja",
+		link: "/sl",
+		dropdown: [
+			{ name: "Začetni Tečaj", link: "#" },
+			{ name: "Nadaljevalni Tečaj", link: "#" },
+			{ name: "Individualni Tečaj", link: "#" },
+			{ name: "Postani Voditelj Čolna", link: "#" }
+		]
+	},
+	strips: {
+		name: "Jadralski Izleti",
+		link: "/sl",
+		dropdown: [
+			{ name: "4-urni Izlet", link: "#" },
+			{ name: "1-dnevni Izlet", link: "#" },
+			{ name: "2-dnevni Izlet", link: "#" },
+			{ name: "3-dnevni Izlet", link: "#" },
+			{ name: "Tedenski Izlet", link: "#" }
+		]
+	},
 	tbuild: { name: "Teambuilding", link: "/sl" },
-	slit: { name: "Literatura", link: "/sl" },
+	slit: {
+		name: "Literatura",
+		link: "/sl",
+		dropdown: [
+			{ name: "Navigacija", link: "#" },
+			{ name: "Manevriranje", link: "#" },
+			{ name: "Vzdrževanje", link: "#" }
+		]
+	},
 	about: { name: "O Nas", link: "/sl/about" },
 	language: { name: "Language" }
 };
@@ -15,14 +42,14 @@ const global = {
 module.exports = {
 	Home: {
 		layout: "main",
-		title: "Domov",
+		title: "SeaStorm Sailing",
 		css: "/style/homepage.css",
 		navigation,
 		heading1: "Doživite z nami",
 		expo1: {
 			title: "Šola Jadranja",
 			text:
-				"Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.",
+				"Ponujam izobraževanja v več različnih okusih za bolj, manj in neizkušene. Pripravil sem začetni, nadaljevalni in individualni tečaj za vse ljubitelje športa. Za vse ki želite narediti korak dalje, pa nudim tečaj za voditelja čolna.",
 			more: global.more1
 		},
 		expo2: {
